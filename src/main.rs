1eedac04edfe486b91f9b9b56f588b934554c7f5
Fenhl <fenhl@fenhl.net>
2021-02-01 00:50:57 +0000

    Fix links to releases pages

--- src/main.rs
+++ src/main.rs
@@ -228,9 +228,9 @@ async fn main() -> Result<Menu, Error> {
                     menu.push(ContentItem::new("Send Pull Request to Homebrew").command(bitbar::Command::terminal(("brew", "bump-cask-pr", "--version", latest, "bitbar"))).into());
                 }
                 menu.push(ContentItem::new("Open GitHub Release").href(if is_swiftbar() {
-                    "https://api.github.com/repos/swiftbar/SwiftBar/releases/latest"
+                    "https://github.com/swiftbar/SwiftBar/releases/latest"
                 } else {
-                    "https://api.github.com/repos/matryer/bitbar/releases/latest"
+                    "https://github.com/matryer/bitbar/releases/latest"
                 }).expect("failed to parse GitHub latest release URL").into());
                 if !is_swiftbar() { //TODO also enable for SwiftBar once that is on Homebrew
                     menu.push(ContentItem::new("Hide Until Homebrew Is Updated").command((current_exe.display(), "hide_until_homebrew_gt", homebrew)).into());
